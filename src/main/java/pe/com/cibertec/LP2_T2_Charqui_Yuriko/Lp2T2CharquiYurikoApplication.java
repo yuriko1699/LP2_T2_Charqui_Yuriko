980e1dbc519bd9938872620ea7bf5f62ee68ec56
yuriko1699 <yurikoaguilar507@gmail.com>
2024-10-13 17:45:03 -0500

    Estructura del proyecto

---
+++ src/main/java/pe/com/cibertec/LP2_T2_Charqui_Yuriko/Lp2T2CharquiYurikoApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_T2_Charqui_Yuriko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2CharquiYurikoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2CharquiYurikoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/LP2_T2_Charqui_Yuriko/Lp2T2CharquiYurikoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_T2_Charqui_Yuriko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2CharquiYurikoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
